cba0d7b52d3ff13389791cd1264269d751dc2f12
Federica D'Elia <federica.delia@redturtle.it>
2013-02-04 16:47:12 +0100

    fix translation

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ setup(name='ospfe.percorso_sla',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'Products.PloneFormGen',
+          'uwosh.pfg.d2c'
           # -*- Extra requirements: -*-
       ],
       entry_points="""
