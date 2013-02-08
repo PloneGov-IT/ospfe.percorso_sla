735b87460fd7a4adb2afc46e366d12a2a42da44e
Federica D'Elia <federica.delia@redturtle.it>
2013-02-08 12:10:36 +0100

    Modifica a vista paziente

--- ospfe/percorso_sla/browser/patient_view.py
+++ ospfe/percorso_sla/browser/patient_view.py
@@ -5,4 +5,15 @@ class PatientView(BrowserView):
         
     def __init__(self, context, request):
         BrowserView.__init__(self, context, request)
+    
+    @property
+    def portal_catalog(self):
+        return getToolByName(self.context, 'portal_catalog')
+        
+    def getSlaFormsPatient(self):
+        patient_path = '/'.join(self.context.getPhysicalPath())
+        return self.portal_catalog.searchResults(portal_type="sla-form",
+                                                 path={'query': patient_path})
+        
+        
 
