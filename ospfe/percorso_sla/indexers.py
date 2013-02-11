d03bf3fbe879e38e0094374080b13a69a8cc59ca
Federica D'Elia <federica.delia@redturtle.it>
2013-02-11 18:24:51 +0100

    Aggiunto supporto per color collection

--- ospfe/percorso_sla/indexers.py
+++ ospfe/percorso_sla/indexers.py
@@ -1,6 +1,13 @@
 from plone.indexer.decorator import indexer
+from Products.CMFCore.utils import getToolByName
 from uwosh.pfg.d2c.interfaces import IFormSaveData2ContentEntry
 
 @indexer(IFormSaveData2ContentEntry)
 def color(object, **kw):
-    return 'red'
+    wtool = getToolByName(object, "portal_workflow")
+    wf_state = wtool.getInfoFor(object,'review_state')
+    if wf_state in ['red','yellow','green']:
+        return wf_state
+    else:
+        return ''
+        
