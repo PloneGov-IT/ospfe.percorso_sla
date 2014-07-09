494e588e7a9d8fd9a4fc34be572a5d73f3305f8d
Andrea Cecchi <andrea.cecchi85@gmail.com>
2014-07-09 10:17:52 +0200

    Plone 4.3 compatibility

--- ospfe/percorso_sla/browser/patient_view.py
+++ ospfe/percorso_sla/browser/patient_view.py
@@ -1,28 +1,34 @@
 from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
-from zope.app.component.hooks import getSite
+try:
+    # Plone < 4.3
+    from zope.app.component.hooks import setSite
+except ImportError:
+    # Plone >= 4.3
+    from zope.component.hooks import setSite
+
 
 class PatientView(BrowserView):
-        
+
     def __init__(self, context, request):
         BrowserView.__init__(self, context, request)
-    
+
     @property
     def portal_catalog(self):
         return getToolByName(self.context, 'portal_catalog')
-        
+
     def getSlaFormsPatient(self):
         patient_path = '/'.join(self.context.getPhysicalPath())
         return self.portal_catalog.searchResults(portal_type="sla-form",
                                                  path={'query': patient_path},
                                                  sort_on='created', sort_order='reverse')
-        
+
     def getFormsFolderPatient(self):
         patient_path = '/'.join(self.context.getPhysicalPath())
         return self.portal_catalog.searchResults(portal_type="FormFolder",
                                                  path={'query': patient_path},
                                                  sort_on='sortable_title', sort_order='ascending')
-        
+
     def check_user_can_fill_form(self, content):
         portal_membership = getToolByName(self.context, 'portal_membership')
         member = portal_membership.getAuthenticatedMember()
